b0672fcc2c4fea662388ad2dab234dbeb93af2a0
CarrieForle <53133715+CarrieForle@users.noreply.github.com>
2026-07-18 21:31:15 +0800

    revert to the old approach: recording tmpros and patch after game start

--- CustomFont/CustomFontPlugin.cs
+++ CustomFont/CustomFontPlugin.cs
@@ -1,5 +1,8 @@
 /*
-Skip cutscene prompt in the title is UnityEngine.UI.Text
+Notes:
+- Skip cutscene prompt in the title is UnityEngine.UI.Text
+- Can't patch in Awake() because some gameobjects instantiate from others and cause the loss of track of original gameobject
+- There is a check in setters of tmpro.fontSize or tmpro.font to check if value has changed, so no penalty for assignment
 */
 
 using System;
@@ -14,9 +17,12 @@ using BepInEx.Logging;
 using HarmonyLib;
 using TeamCherry.Localization;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using static CustomFont.CustomFontPlugin;
 
 namespace CustomFont;
+
+using CachedTMPros = Dictionary<TMProOld.TextMeshPro, (float FontSize, TMProOld.TMP_FontAsset Font)>;
 
 [BepInDependency("org.silksong-modding.i18n")]
 [BepInAutoPlugin(id: "io.github.carrieforle.customfont")]
@@ -27,7 +33,10 @@ partial class CustomFontPlugin : BaseUnityPlugin, IDisposable
     private static ConfigEntry<bool> configAllCharsAtlas;
     internal static ManualLogSource logger;
     internal static TMProOld.TMP_FontAsset? fontAsset;
-    internal static Dictionary<TMProOld.TextMeshPro, (float FontScale, TMProOld.TMP_FontAsset Font)> oldFonts = [];
+    internal static CachedTMPros oldTMPros = [];
+
+    // Some cfbls can't be patched immediately e.g., in Quest menu, that will otherwise cause font size to be applied twice. The reason is unknown but I'm too fed up to find out why.
+    internal static bool cfblCanPatch = true;
     private static string[] fontPaths = [];
     private static string? currentFontPath;
     private FileSystemWatcher fontWatcher;
@@ -45,14 +54,14 @@ partial class CustomFontPlugin : BaseUnityPlugin, IDisposable
         ];
 
         fontWatcher = new(assemblyPath)
-		{
-			NotifyFilter = NotifyFilters.CreationTime |
+        {
+            NotifyFilter = NotifyFilters.CreationTime |
                 NotifyFilters.FileName |
                 NotifyFilters.LastWrite |
                 NotifyFilters.Size
-		};
+        };
 
-		fontWatcher.Changed += (s, e) => OnFileChanged(e.FullPath, e.FullPath);
+        fontWatcher.Changed += (s, e) => OnFileChanged(e.FullPath, e.FullPath);
         fontWatcher.Created += (s, e) => OnFileChanged(null, e.FullPath);
         fontWatcher.Deleted += (s, e) => OnFileChanged(e.FullPath, null);
         fontWatcher.Renamed += (s, e) => OnFileChanged(e.OldFullPath, e.FullPath);
@@ -84,18 +93,7 @@ partial class CustomFontPlugin : BaseUnityPlugin, IDisposable
 
         configReplaceFontMode.SettingChanged += (s, e) =>
         {
-            TryUnpatchTMPros();
-
-            if (configReplaceFontMode.Value == ReplaceFontMode.All)
-            {
-                TryPatchTMPros();
-            }
-
-            var cfbls = FindObjectsByType<ChangeFontByLanguage>(FindObjectsSortMode.None);
-            foreach (var cfbl in cfbls)
-            {
-                cfbl.SetFont();
-            }
+            TryUpdateTMPros();
         };
 
         configFontScale.SettingChanged += (s, e) =>
@@ -106,22 +104,7 @@ partial class CustomFontPlugin : BaseUnityPlugin, IDisposable
                 configFontScale.Value = 1f;
             }
 
-            if (configReplaceFontMode.Value != ReplaceFontMode.Disabled)
-            {
-                var cfbls = FindObjectsByType<ChangeFontByLanguage>(FindObjectsSortMode.None);
-                foreach (var cfbl in cfbls)
-                {
-                    cfbl.SetFont();
-                }
-
-                if (configReplaceFontMode.Value == ReplaceFontMode.All)
-                {
-                    foreach (var (tmpro, attr) in oldFonts)
-                    {
-                        tmpro.fontSize = attr.FontScale * configFontScale.Value;
-                    }
-                }
-            }
+            TryUpdateTMPros();
         };
 
         configAllCharsAtlas.SettingChanged += (s, e) =>
@@ -167,7 +150,7 @@ partial class CustomFontPlugin : BaseUnityPlugin, IDisposable
         logger.LogInfo($"Loading font at \"{Path.GetFileName(currentFontPath)}\".");
 
         long fileSize = new FileInfo(currentFontPath).Length;
-        if (fileSize > 1e6)
+        if (fileSize > 1e6 && configAllCharsAtlas.Value)
         {
             logger.LogWarning($"Font file is large (> {fileSize * 1e-6:F1}MB)! It may take several minutes to to create font atlas.");
         }
@@ -207,6 +190,19 @@ partial class CustomFontPlugin : BaseUnityPlugin, IDisposable
 
     internal static void PatchTMPro(TMProOld.TextMeshPro tmpro, float scale, Material sourceMaterial)
     {
+        if (tmpro == null)
+        {
+            oldTMPros.Remove(tmpro);
+            return;
+        }
+
+        tmpro.fontSize = scale;
+
+        if (tmpro.font == fontAsset)
+        {
+            return;
+        }
+
         fontAsset!.fallbackFontAssets = tmpro.font.fallbackFontAssets;
         if (fontAsset.fallbackFontAssets.IsNullOrEmpty())
         {
@@ -217,7 +213,6 @@ partial class CustomFontPlugin : BaseUnityPlugin, IDisposable
             fontAsset.fallbackFontAssets.Insert(0, tmpro.font);
         }
 
-        tmpro.fontSize = scale;
         tmpro.font = fontAsset;
 
         Debug.Assert(fontAsset.material == tmpro.fontSharedMaterial);
@@ -226,46 +221,65 @@ partial class CustomFontPlugin : BaseUnityPlugin, IDisposable
         tmpro.fontSharedMaterial = oldMaterial;
     }
 
-    internal static void TryPatchTMPros()
+    private static void UnpatchTMPro(TMProOld.TextMeshPro tmpro, (float FontSize, TMProOld.TMP_FontAsset Font) attr)
     {
-        if (fontAsset == null || configReplaceFontMode.Value != ReplaceFontMode.All)
+        if (tmpro == null)
         {
+            oldTMPros.Remove(tmpro);
             return;
         }
+        var a = tmpro == null ? "null" : tmpro.GetInstanceID().ToString();
+        var b = tmpro.font == null ? "null" : tmpro.font.name;
+        var c = attr.Font == null ? "null" : attr.Font.name;
 
-        logger.LogInfo($"Patching {oldFonts.Count} TMPro");
-        oldFonts = oldFonts
-            .Where(t => t.Key != null)
-            .ToDictionary(t => t.Key, t => t.Value);
-
-        foreach (var (tmpro, attr) in oldFonts)
+        if (a == "null" || b == "null" || c == "null")
         {
-            PatchTMPro(tmpro, attr.FontScale * configFontScale.Value, tmpro.fontSharedMaterial);
+            logger.LogInfo($"({a} ,{b}, {c})");
+        }
+
+        tmpro.font = attr.Font;
+        tmpro.fontSize = attr.FontSize;
+    }
+
+    internal static void TryUnpatchTMPro(TMProOld.TextMeshPro tmpro)
+    {
+        if (oldTMPros.TryGetValue(tmpro, out var attr))
+        {
+            UnpatchTMPro(tmpro, attr);
         }
     }
 
-    internal static void UnpatchTMPro(TMProOld.TextMeshPro tmpro)
-    {
-        var fontAttr = oldFonts[tmpro];
-        tmpro.font = fontAttr.Font;
-        tmpro.fontSize = fontAttr.FontScale;
-    }
-
-    internal static void TryUnpatchTMPros()
+    internal static void TryUpdateTMPros()
     {
         if (fontAsset == null)
         {
             return;
         }
 
-        logger.LogInfo($"Unpatching {oldFonts.Count} TMPro");
-        oldFonts = oldFonts
-            .Where(t => t.Key != null)
-            .ToDictionary(t => t.Key, t => t.Value);
-
-        foreach (var (tmpro, _) in oldFonts)
+        if (configReplaceFontMode.Value != ReplaceFontMode.All)
         {
-            UnpatchTMPro(tmpro);
+            logger.LogDebug($"Unpatching TMPro");
+
+            foreach (var (tmpro, attr) in oldTMPros)
+            {
+                UnpatchTMPro(tmpro, attr);
+            }
+        }
+        else
+        {
+            logger.LogDebug($"Patching TMPro");
+
+            foreach (var (tmpro, attr) in oldTMPros)
+            {
+                PatchTMPro(tmpro, attr.FontSize * configFontScale.Value, tmpro.fontSharedMaterial);
+            }
+        }
+
+        var cfbls = FindObjectsByType<ChangeFontByLanguage>(FindObjectsSortMode.None);
+
+        foreach (var cfbl in cfbls)
+        {
+            cfbl.SetFont();
         }
     }
 
@@ -300,48 +314,221 @@ static class Patch
 {
     [HarmonyPostfix]
     [HarmonyPriority(Priority.HigherThanNormal)]
-    [HarmonyPatch(typeof(ChangeFontByLanguage), nameof(ChangeFontByLanguage.SetFont), [])]
-    private static void SetFont(ChangeFontByLanguage __instance)
+    [HarmonyPatch(typeof(GameManager), nameof(GameManager.LevelActivated))]
+    private static void AddTMProsInScene()
+    {
+        logger.LogInfo("Entering Scene");
+        cfblCanPatch = true;
+        if (fontAsset == null)
+        {
+            return;
+        }
+
+        var scene = SceneManager.GetActiveScene();
+        var tmpros = scene.GetRootGameObjects()
+            .SelectMany(go => go.GetComponentsInChildren<TMProOld.TextMeshPro>(true));
+
+        var tmprosAdded = false;
+        foreach (var tmpro in tmpros)
+        {
+            if (tmpro.fontSharedMaterial == null || tmpro.font == null)
+            {
+                continue;
+            }
+
+            var parent = tmpro.transform.parent.gameObject;
+
+            // ignore letter in button prompt
+            if (parent != null && parent.GetComponent<MenuButtonIcon>() != null)
+            {
+                continue;
+            }
+
+            tmprosAdded = tmprosAdded || oldTMPros.TryAdd(tmpro, (tmpro.fontSize, tmpro.font));
+        }
+
+        if (tmprosAdded)
+        {
+            logger.LogDebug("Found scene tmpros");
+            TryUpdateTMPros();
+        }
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPriority(Priority.HigherThanNormal)]
+    [HarmonyPatch(typeof(OpeningSequence), nameof(OpeningSequence.Start))]
+    // Act cards (ACT I Pharloom, etc) are not cfbl, despite them differ between languages.
+    private static void AddTMProsInActCard(OpeningSequence __instance)
     {
         if (fontAsset == null || configReplaceFontMode.Value == ReplaceFontMode.Disabled)
         {
             return;
         }
 
-        PatchTMPro(__instance.tmpro, __instance.tmpro.fontSize * configFontScale.Value, __instance.defaultMaterial);
+        logger.LogDebug("Patching act cards");
+        var tmpros = __instance.GetComponentsInChildren<TMProOld.TextMeshPro>(true);
+        foreach (var tmpro in tmpros)
+        {
+            // We just patch them immediately since they are destroyed immediately after fadeout.
+            PatchTMPro(tmpro, tmpro.fontSize * configFontScale.Value, tmpro.fontSharedMaterial);
+        }
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPriority(Priority.HigherThanNormal)]
+    [HarmonyPatch(typeof(CinematicPlayer), nameof(CinematicPlayer.FinishInGameVideo))]
+    // Act II are not cfbl, despite them differ between languages.
+    private static void AddTMProsInActCard(CinematicPlayer __instance)
+    {
+        if (fontAsset == null || __instance.actCard == null || configReplaceFontMode.Value == ReplaceFontMode.Disabled)
+        {
+            return;
+        }
+
+        logger.LogDebug("Patching act cards");
+        var tmpros = __instance.actCard.GetComponentsInChildren<TMProOld.TextMeshPro>(true);
+        foreach (var tmpro in tmpros)
+        {
+            // We just patch them immediately since they are destroyed immediately after fadeout.
+            PatchTMPro(tmpro, tmpro.fontScale * configFontScale.Value, tmpro.fontSharedMaterial);
+        }
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPriority(Priority.HigherThanNormal)]
+    [HarmonyPatch(typeof(SimpleShopMenu), nameof(SimpleShopMenu.Awake))]
+    // Price tags are only instantiated when Hornet interact with the shop.
+    private static void AddTMProsInShop(SimpleShopMenu __instance)
+    {
+        if (fontAsset == null)
+        {
+            return;
+        }
+
+        __instance.pane.OnPaneStart += () =>
+        {
+            var tmpros = __instance.GetComponentsInChildren<TMProOld.TextMeshPro>(true);
+
+            bool tmprosAdded = false;
+            foreach (var tmpro in tmpros)
+            {
+                if (tmpro.fontSharedMaterial == null || tmpro.font == null)
+                {
+                    continue;
+                }
+
+                tmprosAdded = tmprosAdded || oldTMPros.TryAdd(tmpro, (tmpro.fontSize, tmpro.font));
+            }
+
+            if (tmprosAdded)
+            {
+                logger.LogDebug("Found shop price tags");
+                TryUpdateTMPros();
+            }
+        };
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPriority(Priority.HigherThanNormal)]
+    [HarmonyPatch(typeof(ChangeFontByLanguage), nameof(ChangeFontByLanguage.SetFont), [])]
+    private static void SetFont(ChangeFontByLanguage __instance)
+    {
+        var tmpro = __instance.tmpro;
+        if (tmpro == null || fontAsset == null || !cfblCanPatch)
+        {
+            // The original SetFont will restore the font and font size.
+            // We can do nothing.
+            return;
+        }
+
+        oldTMPros.TryAdd(tmpro, (tmpro.fontSize, tmpro.font));
+        if (configReplaceFontMode.Value != ReplaceFontMode.Disabled)
+        {
+            PatchTMPro(tmpro, oldTMPros[tmpro].FontSize * configFontScale.Value, __instance.defaultMaterial);
+        }
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPriority(Priority.HigherThanNormal)]
+    [HarmonyPatch(typeof(QuestItemManager), nameof(QuestItemManager.ResetExtraDisplay))]
+    [HarmonyPatch(typeof(QuestItemManager), nameof(QuestItemManager.SetDisplay), [typeof(InventoryItemSelectable)])]
+    private static void PatchFontSize(QuestItemManager __instance)
+    {
+        if (fontAsset == null)
+        {
+            // This is called every time the player opens quest, where fontSize is always reset.
+            return;
+        }
+
+        if (oldTMPros.TryGetValue(__instance.descriptionText, out var attr))
+        {
+            if (configReplaceFontMode.Value == ReplaceFontMode.Disabled)
+            {
+                __instance.descriptionText.fontSize = attr.FontSize;
+            }
+            else
+            {
+                __instance.descriptionText.fontSize = attr.FontSize * configFontScale.Value;
+            }
+        }
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(UIManager), nameof(UIManager.Start))]
+    private static void ClearTMPros()
+    {
+        logger.LogDebug("Clearing TMPros");
+        cfblCanPatch = false;
+        oldTMPros.Clear();
     }
 
     [HarmonyPostfix]
     [HarmonyPriority(Priority.HigherThanNormal)]
     [HarmonyPatch(typeof(GameManager), nameof(GameManager.ContinueGame))]
     [HarmonyPatch(typeof(GameManager), nameof(GameManager.StartNewGame))]
-    private static void PatchTMPros()
+    private static void StartGame()
     {
+        GameCameras.SilentInstance.hudCamera.gameObject
+            .GetComponentsInChildren<TMProOld.TextMeshPro>(true)
+            .Do(tmpro => oldTMPros.TryAdd(tmpro, (tmpro.fontSize, tmpro.font)));
+
         TryLoadFont();
-        TryPatchTMPros();
     }
 
     [HarmonyPostfix]
     [HarmonyPriority(Priority.HigherThanNormal)]
-    [HarmonyPatch(typeof(TMProOld.TextMeshPro), nameof(TMProOld.TextMeshPro.Awake))]
-    private static void RecordAndPatchTMPro(TMProOld.TextMeshPro __instance)
+    [HarmonyPatch(typeof(TMProOld.TextMeshPro), "OnDestroy")]
+    private static void RemoveTMPro(TMProOld.TextMeshPro __instance)
     {
-        oldFonts.TryAdd(__instance, (__instance.fontSize, __instance.font));
+        oldTMPros.Remove(__instance);
+    }
 
-        if (fontAsset == null || configReplaceFontMode.Value != ReplaceFontMode.All)
+    [HarmonyPrefix]
+    [HarmonyPriority(Priority.HigherThanNormal)]
+    [HarmonyPatch(typeof(InventoryItemListManager<InventoryItemCollectable, CollectableItem>), nameof(InventoryItemListManager<,>.UpdateList))]
+    private static void UnpatchInventory(InventoryItemListManager<InventoryItemCollectable, CollectableItem> __instance)
+    {
+        var tmpros = __instance.GetComponentsInChildren<TMProOld.TextMeshPro>(true);
+
+        foreach (var tmpro in tmpros)
         {
-            return;
+            TryUnpatchTMPro(tmpro);
+        }
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPriority(Priority.HigherThanNormal)]
+    [HarmonyPatch(typeof(InventoryItemListManager<InventoryItemCollectable, CollectableItem>), nameof(InventoryItemListManager<,>.UpdateList))]
+    private static void PatchInventory(InventoryItemListManager<InventoryItemCollectable, CollectableItem> __instance)
+    {
+        var tmpros = __instance.GetComponentsInChildren<TMProOld.TextMeshPro>(true);
+
+        foreach (var tmpro in tmpros)
+        {
+            oldTMPros.TryAdd(tmpro, (tmpro.fontSize, tmpro.font));
         }
 
-        PatchTMPro(__instance, __instance.fontSize * configFontScale.Value, __instance.fontSharedMaterial);
-    }
-
-    [HarmonyPostfix]
-    [HarmonyPriority(Priority.HigherThanNormal)]
-    [HarmonyPatch(typeof(ChangeFontByLanguage), nameof(ChangeFontByLanguage.Awake))]
-    private static void ExcludeCfbl(ChangeFontByLanguage __instance)
-    {
-        oldFonts.Remove(__instance.tmpro);
+        TryUpdateTMPros();
     }
 }
 
